ac0be6ccb9668ccdd4015d52b25db8fdc1d96d9c
Király Sándor <kiralysandor1986@gmail.com>
2025-05-05 21:03:29 +0200

    - update docker setup sh

--- docker.sh
+++ docker.sh
@@ -3,4 +3,5 @@
 docker build -t pigonbot .
 
 # replace chatbot if you want to rename the container
-docker run --env-file .env --name chatbot --mount type=bind,src=.,dst=/data,rw pigonbot
+docker run -d --env-file .env --name chatbot --mount type=bind,source=.,target=/data pigonbot
+
